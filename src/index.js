1a974aed9c5142af4b61fd2f4cc407eaf89d1904
Гулов Анджей Сергеевич <a.s.gulov@ANOITR.local>
2020-06-03 16:51:48 +0300

    init

--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
-import React from 'react';
+import React, {useEffect, useState} from "react";
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+const getPlanet = (id) => {
+    return fetch(`https://swapi.dev/api/planets/${id}/`)
+        .then(res => res.json())
+        .then(data => data);
+}
+
+const useRequest = (request) => {
+    const [ dataState, setDataState ] = useState(null);
+
+    useEffect(() => {
+       let cancelled = false;
+       request()
+           .then(data => !cancelled && setDataState(data));
+       return () => cancelled = true;
+    }, [ request ])
+    return dataState;
+}
+
+const usePlanetInfo = (id) => {
+    const request = () => getPlanet(id);
+    return useRequest(request);
+}
+
+const PlanetInfo = ({id}) => {
+    const data = usePlanetInfo(id);
+    return (
+        <div>{id} - {data && data.name}</div>
+    );
+}
+
+const App = () => {
+    const [value, setValue] = useState(1);
+    return (
+        <div>
+            <button onClick={() => setValue((v) => v+1)}> + </button>
+            <PlanetInfo id={value}/>
+        </div>
+    )
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'));
